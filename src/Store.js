7e4f9a3b8591a34f096da417e43fde61f365c0d9
Harsh <hk755063@gmail.com>
2024-06-24 10:28:20 +0530

    redux added

---
+++ src/Store.js
@@ -0,0 +1,19 @@
+import {createStore} from 'redux';
+
+function cartReducer(state = {items : {}}, action){
+    switch(action.type){
+        case "ADD_TO_CART":{
+
+        }
+        case "REMOVE_FROM_CART":{
+
+        }
+        default:
+            return state;
+    }
+
+}
+
+const store = createStore(cartReducer);
+
+export default store;
